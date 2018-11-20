c42bc6cdecab73c6735be97e28418d1bd514dd80
Darren Lester <darren@darrenlester.com>
2018-11-20 20:34:44 +0000

    Extract webhook endpoint into separate router

--- app.js
+++ app.js
@@ -2,57 +2,14 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const messaging = require('./messaging');
+
+const webhookRouter = require('./routers/webhook');
 const statusRouter = require('./routers/status');
 
 const app = express();
 app.use(bodyParser.json());
 
-// fb integration validation
-app.get('/webhook', function(req, res) {
-  if (
-    req.query['hub.mode'] === 'subscribe' &&
-    req.query['hub.verify_token'] === process.env.VALIDATION_TOKEN
-  ) {
-    console.log('Validating webhook');
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    console.error('Failed validation. Make sure the validation tokens match.');
-    res.sendStatus(403);
-  }
-});
-
-// fb messages will be sent here
-app.post('/webhook', function(req, res) {
-  const data = req.body;
-
-  // Make sure this is a page subscription
-  if (data.object == 'page') {
-    // Iterate over each entry
-    // There may be multiple if batched
-    data.entry.forEach(function(pageEntry) {
-      // Iterate over each messaging event
-      pageEntry.messaging.forEach(function(messagingEvent) {
-        if (messagingEvent.optin) {
-          messaging.receivedAuthentication(messagingEvent);
-        } else if (messagingEvent.message) {
-          messaging.receivedMessage(messagingEvent);
-        } else if (messagingEvent.delivery) {
-          messaging.receivedDeliveryConfirmation(messagingEvent);
-        } else if (messagingEvent.postback) {
-          messaging.receivedPostback(messagingEvent);
-        } else {
-          console.log(
-            'Webhook received unknown messagingEvent: ',
-            messagingEvent
-          );
-        }
-      });
-    });
-    res.sendStatus(200);
-  }
-});
-
+app.use('/webhook', webhookRouter);
 app.use('/status', statusRouter);
 
 module.exports = app;
---
+++ routers/webhook.js
@@ -0,0 +1,52 @@
+const express = require('express');
+
+const messaging = require('./messaging');
+
+const webhookRouter = express.Router();
+
+// fb integration validation
+webhookRouter.get('/', function(req, res) {
+  if (
+    req.query['hub.mode'] === 'subscribe' &&
+    req.query['hub.verify_token'] === process.env.VALIDATION_TOKEN
+  ) {
+    console.log('Validating webhook');
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error('Failed validation. Make sure the validation tokens match.');
+    res.sendStatus(403);
+  }
+});
+
+// fb messages will be sent here
+webhookRouter.post('/', function(req, res) {
+  const data = req.body;
+
+  // Make sure this is a page subscription
+  if (data.object == 'page') {
+    // Iterate over each entry
+    // There may be multiple if batched
+    data.entry.forEach(function(pageEntry) {
+      // Iterate over each messaging event
+      pageEntry.messaging.forEach(function(messagingEvent) {
+        if (messagingEvent.optin) {
+          messaging.receivedAuthentication(messagingEvent);
+        } else if (messagingEvent.message) {
+          messaging.receivedMessage(messagingEvent);
+        } else if (messagingEvent.delivery) {
+          messaging.receivedDeliveryConfirmation(messagingEvent);
+        } else if (messagingEvent.postback) {
+          messaging.receivedPostback(messagingEvent);
+        } else {
+          console.log(
+            'Webhook received unknown messagingEvent: ',
+            messagingEvent
+          );
+        }
+      });
+    });
+    res.sendStatus(200);
+  }
+});
+
+module.exports = webhookRouter;
