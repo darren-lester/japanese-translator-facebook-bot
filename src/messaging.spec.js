366cfa6a0d49666117c23b2932b25b08f2cda294
Darren Lester <darren@darrenlester.com>
2018-11-27 21:46:46 +0000

    Add test for messaging.receivedMessage sending translation to Facebook

---
+++ src/messaging.spec.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const nock = require('nock');
+
+const messaging = require('./messaging');
+
+process.env.TRANSLATOR = 'http://translator';
+
+nock(process.env.TRANSLATOR)
+  .get('/?input=hello&source=&target=ja')
+  .reply(200, {
+    translation: 'こんにちは'
+  });
+
+const facebook = nock('https://graph.facebook.com')
+  .post('/v2.6/me/messages', {
+    recipient: {
+      id: 1
+    },
+    message: {
+      text: 'こんにちは'
+    }
+  })
+  .reply(200, {
+    recipient_id: '1',
+    message_id: '123'
+  });
+
+describe('messaging', () => {
+  describe('receivedMessage', () => {
+    const event = {
+      message: {
+        text: 'hello'
+      },
+      recipient: {
+        id: 1
+      },
+      sender: {
+        id: 1
+      }
+    };
+
+    test('sends translation to Facebook', async () => {
+      await messaging.receivedMessage(event);
+      expect(facebook.isDone()).toBe(true);
+    });
+  });
+});
