ed08ab65fef7e633df5de441b7219dd33f1c7fd6
Darren Lester <darren@darrenlester.com>
2018-12-03 20:11:10 +0000

    Extract receivedMessage into separate module

--- src/messaging/messaging.js
+++ src/messaging/messaging.js
@@ -1,53 +1,9 @@
 'use strict';
 
-const translator = require('japanese-translator-interface');
-const messages = require('./messages');
-const facebook = require('../facebook');
-const createMessage = require('./create-message');
-const isTextMessage = require('./is-text-message');
 const receivedAuthentication = require('./received-authentication');
 const receivedDeliveryConfirmation = require('./received-delivery-confirmation');
 const receivedPostback = require('./received-postback');
-
-async function receivedMessage(event) {
-  const senderID = event.sender.id;
-  const recipientID = event.recipient.id;
-  const timeOfMessage = event.timestamp;
-  const message = event.message;
-
-  console.log(
-    `Received message for user ${senderID} and page ${recipientID} at ${timeOfMessage} with message:`
-  );
-  console.log(JSON.stringify(message));
-
-  if (isTextMessage(message)) {
-    const { text } = message;
-    switch (text) {
-      case ':help':
-        return sendHelpMessage(senderID);
-        break;
-
-      default:
-        return sendTranslatedMessage(senderID, text);
-    }
-  } else {
-    const message = createMessage(senderId, messages.onlyText);
-    return facebook.send(message);
-  }
-}
-
-async function sendTranslatedMessage(recipientId, messageText) {
-  const message = createMessage(recipientId);
-  const translatorResponse = await translator.translate(messageText);
-  const json = await translatorResponse.json();
-  message.message.text = json.translation;
-  return facebook.send(message);
-}
-
-async function sendHelpMessage(recipientId) {
-  const message = createMessage(recipientId, messages.help);
-  return facebook.send(message);
-}
+const receivedMessage = require('./received-message');
 
 module.exports = {
   receivedAuthentication,
